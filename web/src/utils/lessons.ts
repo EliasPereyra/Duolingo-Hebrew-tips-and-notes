0a2f1542a7fc585be500f8360d9576e300e33fd6
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-07-08 23:34:39 -0300

    Create a utils folder

---
+++ web/src/utils/lessons.ts
@@ -0,0 +1,272 @@
+interface Lesson {
+  title: string;
+  link: string;
+  body?: string;
+}
+
+export const lessons: Lesson[] = [
+  {
+    title: "Letters 1",
+    link: "/lessons/letters-1",
+  },
+  {
+    title: "Letters 2",
+    link: "/lessons/letters-2",
+  },
+  {
+    title: "Letters 3",
+    link: "/lessons/letters-3",
+  },
+  {
+    title: "Common Phrases",
+    link: "/lessons/common-phrases",
+  },
+  {
+    title: "Basics",
+    link: "/lessons/basics",
+  },
+  {
+    title: "There is/To Have",
+    link: "/lessons/there-is-to-have",
+  },
+  {
+    title: "Introduction to adjectives",
+    link: "/lessons/introduction-to-adjectives",
+  },
+  {
+    title: "Food 1",
+    link: "/lessons/food-1",
+  },
+  {
+    title: "Animals",
+    link: "/lessons/animals",
+  },
+  {
+    title: "Plurals",
+    link: "/lessons/plurals",
+  },
+  {
+    title: "Possessives 1",
+    link: "/lessons/possessives-1",
+  },
+  {
+    title: "Adjectives 1 - Basics",
+    link: "/lessons/adjectives-1-basics",
+  },
+  {
+    title: "Direct Object",
+    link: "/lessons/direct-object",
+  },
+  {
+    title: "Clothing",
+    link: "/lessons/clothing",
+  },
+  {
+    title: "Verbs: Present - Pa'al",
+    link: "/lessons/verbs-present-pa-al",
+  },
+  {
+    title: "Colors",
+    link: "/lessons/colors",
+  },
+  {
+    title: "Prepositions 1",
+    link: "/lessons/prepositions-1",
+  },
+  {
+    title: "Numbers 1",
+    link: "/lessons/numbers-1",
+  },
+  {
+    title: "Questions",
+    link: "/lessons/questions",
+  },
+  {
+    title: "Determiners",
+    link: "/lessons/determiners",
+  },
+  {
+    title: "Occupations",
+    link: "/lessons/occupations",
+  },
+  {
+    title: "Conjunctions",
+    link: "/lessons/conjunctions",
+  },
+  {
+    title: "Prepositions 2",
+    link: "/lessons/prepositions-2",
+  },
+  {
+    title: "Possessives 2",
+    link: "/lessons/possessives-2",
+  },
+  {
+    title: "Verbs: Present - Pi'el",
+    link: "/lessons/verbs-present-pi-el",
+  },
+  {
+    title: "Dates and Time",
+    link: "/lessons/dates-and-time",
+  },
+  {
+    title: "Adjectives 2",
+    link: "/lessons/adjectives-2",
+  },
+  {
+    title: "Adverbs",
+    link: "/lessons/adverbs",
+  },
+  {
+    title: "Family",
+    link: "/lessons/family",
+  },
+  {
+    title: "Home Sweet Home",
+    link: "/lessons/home-sweet-home",
+  },
+  {
+    title: "Construct State 1",
+    link: "/lessons/construct-state-1",
+  },
+  {
+    title: "Infinitives 1",
+    link: "/lessons/infinitives-1",
+  },
+  {
+    title: "Verbs: Present - Hiph'il",
+    link: "/lessons/verbs-present-hiph-il",
+  },
+  {
+    title: "People",
+    link: "/lessons/people",
+  },
+  {
+    title: "Numbers 2",
+    link: "/lessons/numbers-2",
+  },
+  {
+    title: "Modals",
+    link: "/lessons/modals",
+  },
+  {
+    title: "Education",
+    link: "/lessons/education",
+  },
+  {
+    title: "Travel",
+    link: "/lessons/travel",
+  },
+  {
+    title: "Verbs: Past Active 1",
+    link: "/lessons/verbs-past-active-1",
+  },
+  {
+    title: "Comparison",
+    link: "/lessons/comparison",
+  },
+  {
+    title: "Verbs: Present Reflexive - Hitpa'el",
+    link: "/lessons/verbs-present-reflexive-hitpa-el",
+  },
+  {
+    title: "Adjectives 3",
+    link: "/lessons/adjectives-3",
+  },
+  {
+    title: "Imperative 1",
+    link: "/lessons/imperative-1",
+  },
+  {
+    title: "Languages",
+    link: "/lessons/languages",
+  },
+  {
+    title: "Emergency!",
+    link: "/lessons/emergency",
+  },
+  {
+    title: "Verbs: Future Active 1",
+    link: "/lessons/verbs-future-active-1",
+  },
+  {
+    title: "Passive: Pa'ul, Pu'al and Huf'al",
+    link: "/lessons/passive-pa-ul-pu-al-and-huf-al",
+  },
+  {
+    title: "Nif'al Construction",
+    link: "/lessons/nif-al-construction",
+  },
+  {
+    title: "Nature",
+    link: "/lessons/nature",
+  },
+  {
+    title: "Discussions and decisions",
+    link: "/lessons/discussions-and-decisions",
+  },
+  {
+    title: "Imperative 2",
+    link: "/lessons/imperative-2",
+  },
+  {
+    title: "Verbs: Past Active 2",
+    link: "/lessons/verbs-past-active-2",
+  },
+  {
+    title: "Past Nif'al",
+    link: "/lessons/past-nif-al",
+  },
+  {
+    title: "Infinitives 2",
+    link: "/lessons/infinitives-2",
+  },
+  {
+    title: "Construct State 2",
+    link: "/lessons/construct-state-2",
+  },
+  {
+    title: "The Conditional",
+    link: "/lessons/the-conditional",
+  },
+  {
+    title: "Medical",
+    link: "/lessons/medical",
+  },
+  {
+    title: "Negative Imperatives",
+    link: "/lessons/negative-imperatives",
+  },
+  {
+    title: "Verbal Nouns",
+    link: "/lessons/verbal-nouns",
+  },
+  {
+    title: "Music",
+    link: "/lessons/music",
+  },
+  {
+    title: "Diminutives",
+    link: "/lessons/diminutives",
+  },
+  {
+    title: "Future Nif'al",
+    link: "/lessons/future-nif-al",
+  },
+  {
+    title: "Formal",
+    link: "/lessons/formal",
+  },
+  {
+    title: "Business",
+    link: "/lessons/business",
+  },
+  {
+    title: "Outer Space",
+    link: "/lessons/outer-space",
+  },
+  {
+    title: "Jewish Festivals",
+    link: "/lessons/jewish-festivals",
+  },
+];
