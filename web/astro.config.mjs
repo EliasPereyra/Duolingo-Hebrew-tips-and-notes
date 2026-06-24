9b2b4f4414c07f935d1a3d2f615f6467d3de9b6c
Elias Pereyra <eliaspereyra_gomez@hotmail.com>
2026-06-23 23:06:19 -0300

    fix: delete non existing url from the astro config file

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://eliaspereyra.github.io/Duolingo-Hebrew-tips-and-notes",
   base: "/Duolingo-Hebrew-tips-and-notes",
 });
