ce31fbf57f1b0444d4bea18f5f121aafb37897cb
Elias Pereyra <eliaspereyra_gomez@hotmail.com>
2026-09-14 19:32:40 -0300

    feat: add two custom fonts, one for latin texts and the another one for the hebrew chars

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,6 +1,52 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   base: "/",
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Manrope",
+      cssVariable: "--font-manrope",
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: [
+              "./src/assets/fonts/manrope/static/manrope-latin-400-normal.woff2",
+              "./src/assets/fonts/manrope/static/manrope-latin-ext-400-normal.woff2",
+            ],
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: [
+              "./src/assets/fonts/manrope/static/manrope-latin-700-normal.woff2",
+              "./src/assets/fonts/manrope/static/manrope-latin-ext-700-normal.woff2",
+            ],
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.local(),
+      name: "David Libre",
+      cssVariable: "--font-david-libre",
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/david_libre/davidlibre-regular.woff2"],
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: ["./src/assets/fonts/david_libre/davidlibre-bold.woff2"],
+          },
+        ],
+      },
+    },
+  ],
 });
